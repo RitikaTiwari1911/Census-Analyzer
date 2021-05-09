e33b3064ecbe465e249f580fc743e19d4cf8329e
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-05-09 16:33:00 +0530

    ADDED: Given the state census CSV File when correct but type incorrect return custom exception

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -10,6 +10,10 @@ import java.nio.file.Paths;
 import java.util.Iterator;
 
 public class CensusAnalyser {
+
+    private static final String INDIA_CENSUS_CSV_FILE_PATH ="C:\\Users\\RITIKA\\IdeaProjects\\CensusAnalyser\\src\\test\\resources\\IndiaStateCensusData.csv" ;
+
+    //to load Indian state Census from CSV File
     public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
         try {
             Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
@@ -17,7 +21,8 @@ public class CensusAnalyser {
             csvToBeanBuilder.withType(IndiaCensusCSV.class);
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
             CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
-            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();;
+            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
+            ;
             int namOfEateries = 0;
             while (censusCSVIterator.hasNext()) {
                 namOfEateries++;
@@ -28,5 +33,10 @@ public class CensusAnalyser {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         }
+    }
+
+    public static void main(String[] args) throws CensusAnalyserException {
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
+        censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
     }
 }
